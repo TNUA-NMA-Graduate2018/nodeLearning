7f1b3148be651cede2d5150affa3c33a50d2a8b3
Yungga <cherry841027@gmail.com>
2018-02-24 02:24:04 +0800

    add new test file add node test files

---
+++ homework/yungga/nodeServerTest/wh_09main.js
@@ -0,0 +1,20 @@
+var net = require('net')
+var HOST = '127.0.0.1';
+var PORT = 30009; // 定義 Port
+//建立 net.createServer() 的物件
+
+
+var server = net.createServer(function (socket) {
+
+  // data event： 到收到資料傳輸時觸發事件 ， argument 為對象傳輸的物件
+  socket.on('data', function (data) {
+
+    // write event: 傳輸資料的事件
+
+
+
+  })
+})
+
+
+server.listen(PORT, HOST);
