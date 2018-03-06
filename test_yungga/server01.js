bd70426c3974fa280cf2ffbfb99812af76dbffd4
Yungga <cherry841027@gmail.com>
2018-03-07 01:49:15 +0800

    add

--- test_yungga/server01.js
+++ test_yungga/server01.js
@@ -4,10 +4,10 @@ var server = net.createServer((socket) => {
     //加入一个标志
     //加入到客户列表中
     var name = clients.add(socket);
-
     //接收客户发过来的信息
     socket.on('data', function(data) {
         console.log(data);
+        if data='\n'; return;
         //发送给所有人
         clients.sendAll(socket,data);
     });
@@ -16,15 +16,11 @@ var server = net.createServer((socket) => {
         console.log('client end' + socket.remoteAddress + ' ' + socket.remotePort);
         //删除客户
         clients.del(socket);
-        //发送给所有人
-        clients.sendAll(socket, ' online: ' + clients.length());
 
     });
     console.log('online:'+ clients.length() );
 
-
     socket.write(' welcome' + name + '!\n' );
-    clients.sendAll(socket, 'online: ' + clients.length());
 }).on('error', (err) => {
   // handle errors here
   throw err;
@@ -62,16 +58,16 @@ clients.del = function(socket){
     data   要发送的数据
 */
 clients.sendAll = function(socket,data){
-    console.log('sendtoall');
+    console.log('send to all');
     for(var i=0 ; i < this.list.length ; i++){
         if(socket !== this.list[i]){
             var o = this.list[i];
             //检查socket是否可以写
             if (o.writable) {
                 console.log( o.name);
-                o.write('-----\n' + '\n' + socket.name + ':\n');
+                //o.write('-----\n' + '\n' + socket.name + ':\n');
                 o.write(data);
-                o.write('-----');
+                //o.write('-----');
             } else {
                 console.log('socket lost' + o.name);
                 //socket断开了什么的,就不能发送啦,需要删除这个连接
@@ -87,8 +83,4 @@ clients.length = function(){
 }
 
 //开启聊天服务器~~
-server.listen(({
-  host: '35.194.227.31',
-  port:30001,
-  exclusive: true
-}));
+server.listen(30001);
